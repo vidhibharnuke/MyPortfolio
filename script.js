848920886e871d881903372b6e78acaa9c05190a
vidhibharnuke <127824245+vidhibharnuke@users.noreply.github.com>
2025-06-16 22:03:43 +0530

    New

--- script.js
+++ script.js
@@ -17,6 +17,18 @@
         if (section && section.offsetTop <= fromTop && section.offsetTop + section.offsetHeight > fromTop) {
           navLinks.forEach(l => l.classList.remove('active'));
           document.addEventListener('contextmenu', event => event.preventDefault());
+          document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+  }, false);
+  document.addEventListener('keydown', function(e) {
+    if (
+      (e.ctrlKey && (e.key === 's' || e.key === 'u')) || // Ctrl+S or Ctrl+U
+      (e.key === 'F12') || // F12
+      (e.ctrlKey && e.shiftKey && e.key === 'I') // Ctrl+Shift+I
+    ) {
+      e.preventDefault();
+    }
+  }, false);
           link.classList.add('active');
         }
       });
